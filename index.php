84b61fb41e52a4d1151597301f6fe21e89fdfbb9
BenjaminBeg <benjamin.beguin@modulonet.fr>
2015-06-13 00:49:49 +0200

    test

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+	<head>
+		<meta chaset="utf-8">
+		<script src="js/jquery-2.1.1.js"></script>
+		<script src="js/switch_letter.min.js"></script>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+	
+	<div class="contenair">	
+		<div id="mytxt">
+			
+
+		</div>
+
+	</div>
+		
+	</body>
+
+	<script>
+
+	$(document).ready(function(){
+
+		
+
+		$('#mytxt').letter_swap({
+		  speed: 100,
+		  waitingTime: 2000,
+		  varchar:	["Letter swap" , 'is a javascript pluging', 'with a funny text swap ' , 'animation.' , 'Read the doc', "to know how much it's customizable", 'Easy to use,' ,'free to use.'],
+		  loop : true,
+		  onlylowercase : true		,
+		  onlycaps :false,
+		  NumberOfChange : 5
+		});
+
+
+
+	});
+
+	</script>
+</html>
